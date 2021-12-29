ea110e09b6d5a1b1a0baea4dcd1d3f128ed52659
JeffersonAddai-Poku <jeffepok@gmail.com>
2021-12-29 13:44:01 +0000

    add splash screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/pages/dashboard";
 import Login from './components/pages/login';
+import SplashScreen from "./components/pages/splashScreen";
+import ProtectedRoute from "./components/routes/protectedRoute";
 
 export default function App() {
   return (
     <>
       <Routes>
+        <Route path="" element={<SplashScreen />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/dashboard" element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </>
-  )
-}
+  );
+};
---
+++ src/components/pages/splashScreen.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { useNavigate  } from 'react-router-dom';
+import AuthService from '../../services/auth/auth';
+
+function SplashScreen() {
+    const navigate = useNavigate ();
+    const authService = new AuthService();
+
+    const navigateUser = async () =>{
+        const verified = await authService.verifyToken();
+        if (verified) {
+            navigate("/dashboard");
+        }else {
+            navigate("/login");
+        }
+    }
+    useEffect(()=>{
+        navigateUser();        
+    })
+    return (
+        <div className="container bg-blue-600 h-screen items-center justify-center">
+            <h1 className="text-white my-auto">Jey Inventory</h1>
+        </div>
+    )
+}
+
+export default SplashScreen
