f2d67353b5f3c1e0321febd8f16c143dc85cef78
JeffersonAddai-Poku <jeffepok@gmail.com>
2022-01-06 09:20:20 +0000

    add item service

---
+++ src/services/items/items.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import * as constants from '../../utils/constants';
+import AuthService from '../auth/auth';
+
+export default class ItemsService {
+    async getItems() {
+        const authService = new AuthService();
+        const token = authService.getToken();
+        const url = constants.BACKEND_URL + constants.ITEMS_URL;
+        const config = {
+            headers: { Authorization: `Token ${token}` }
+        };
+        let response;
+        try {
+            response = await axios.get(url, config);
+        }catch(e){
+            response = {error: "An error occurred"};
+        }
+        return response;
+    }
+}
