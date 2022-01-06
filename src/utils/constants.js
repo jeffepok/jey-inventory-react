f362bafe681374aa4ed8bc9d09cad9a2191e2bd9
JeffersonAddai-Poku <jeffepok@gmail.com>
2022-01-06 09:20:50 +0000

    add constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const BACKEND_URL = "https://jey-inventory.herokuapp.com";
 export const LOGIN_URL = "/api/auth/token/login/";
 export const VERIFY_TOKEN_URL = "/api/auth/users/me";
+export const ITEMS_URL = "/api/inventory/items/";
