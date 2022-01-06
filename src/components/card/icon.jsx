eb7dbdff7f4868ac24b01f0f82d096eb717a906c
JeffersonAddai-Poku <jeffepok@gmail.com>
2022-01-06 09:19:28 +0000

    add icon

---
+++ src/components/card/icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Icon() {
+    return (
+        <div>
+            <span className="text-green-500"><i className="fas fa-caret-up"></i></span>   
+        </div>
+    )
+}
+
+export default Icon
